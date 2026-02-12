cc6fae80767d0524b6fafdd737b7c79939c03413
EgorFray <fatamorganaa933@gmail.com>
2026-02-12 12:33:05 +0200

    Fetching data with server side props

--- pages/cabins/[cabinId].js
+++ pages/cabins/[cabinId].js
@@ -1,14 +1,23 @@
+import CabinView from "@/components/CabinView";
+import { getCabin } from "@/lib/data-service";
 import Head from "next/head";
 import { useRouter } from "next/router";
 
-function Cabin() {
-	const router = useRouter();
+export async function getServerSideProps({ params }) {
+	const cabin = await getCabin(params.cabinId);
+	return { props: { cabin }, revalidate:  };
+}
+
+function Cabin(cabin) {
+	// const router = useRouter();
 	return (
 		<>
 			<Head>
-				<title>Cabin {router.query.cabinId} / The wild oasis</title>
+				<title>Cabin {cabin.name} / The wild oasis</title>
 			</Head>
-			<div>Cabin #{router.query.cabinId}</div>
+			<div className="max-w-6xl mx-auto mt-8">
+				<CabinView cabin={cabin} />
+			</div>
 		</>
 	);
 }
